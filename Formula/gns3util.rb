b0c2e48e93599af23bb42499b7a1095ca7bc0b7c
Stefanistkuhl <ghgstefan@gmail.com>
2025-09-11 03:49:33 +0200

    surely fix

--- Formula/gns3util.rb
+++ Formula/gns3util.rb
@@ -1,37 +1,30 @@
 class Gns3util < Formula
   desc "GNS3 API utility for managing GNS3v3 servers"
   homepage "https://github.com/Stefanistkuhl/gns3-api-util"
-  url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.2/gns3util-darwin-amd64.tar.gz"
-  sha256 "a569177a3527a4fd3527afacf8948f0938df15c6416c4c9147e495394b7d6ba3"
   license "GPL-3.0-or-later"
   version "1.0.2"
 
-  # Detect architecture
   if Hardware::CPU.arm?
     url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.2/gns3util-darwin-arm64.tar.gz"
     sha256 "1c4d51414f334b04b5aaecefbc780204207fe9eb547a7f7c15a6fcbf05a0907f"
+  else
+    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.2/gns3util-darwin-amd64.tar.gz"
+    sha256 "a569177a3527a4fd3527afacf8948f0938df15c6416c4c9147e495394b7d6ba3"
   end
 
   def install
-    # Install the binary (rename from platform-specific name to gns3util)
-    if Hardware::CPU.arm?
-      bin.install "gns3util-darwin-arm64" => "gns3util"
-    else
-      bin.install "gns3util-darwin-amd64" => "gns3util"
-    end
-    
-    # Install shell completions if they exist
-    if File.exist?("completions/gns3util.bash")
-      bash_completion.install "completions/gns3util.bash" => "gns3util"
-    end
-    if File.exist?("completions/_gns3util")
-      zsh_completion.install "completions/_gns3util"
-    end
-    if File.exist?("completions/gns3util.fish")
-      fish_completion.install "completions/gns3util.fish"
-    end
-    
-    # Install man pages if they exist
+    # Pick the correct binary inside the tarball
+    binary_name = Hardware::CPU.arm? ? "gns3util-darwin-arm64" : "gns3util-darwin-amd64"
+
+    # Install and rename it to gns3util
+    bin.install binary_name => "gns3util"
+
+    # Shell completions
+    bash_completion.install "completions/gns3util.bash" => "gns3util" if File.exist?("completions/gns3util.bash")
+    zsh_completion.install "completions/_gns3util" if File.exist?("completions/_gns3util")
+    fish_completion.install "completions/gns3util.fish" if File.exist?("completions/gns3util.fish")
+
+    # Man pages
     man1.install "man/gns3util.1" if File.exist?("man/gns3util.1")
   end
 
@@ -39,3 +32,4 @@ class Gns3util < Formula
     system "#{bin}/gns3util", "--help"
   end
 end
+
