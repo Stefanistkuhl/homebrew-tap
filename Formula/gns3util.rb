b05ba63be52af49337013d8d6ff16233a2537801
Stefanistkuhl <ghgstefan@gmail.com>
2025-09-11 04:02:59 +0200

    Update gns3util to v1.0.3

--- Formula/gns3util.rb
+++ Formula/gns3util.rb
@@ -2,14 +2,14 @@ class Gns3util < Formula
   desc "GNS3 API utility for managing GNS3v3 servers"
   homepage "https://github.com/Stefanistkuhl/gns3-api-util"
   license "GPL-3.0-or-later"
-  version "1.0.2"
+  version "1.0.3"
 
   if Hardware::CPU.arm?
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.2/gns3util-darwin-arm64.tar.gz"
-    sha256 "1c4d51414f334b04b5aaecefbc780204207fe9eb547a7f7c15a6fcbf05a0907f"
+    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.3/gns3util-darwin-arm64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   else
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.2/gns3util-darwin-amd64.tar.gz"
-    sha256 "a569177a3527a4fd3527afacf8948f0938df15c6416c4c9147e495394b7d6ba3"
+    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.3/gns3util-darwin-arm64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
