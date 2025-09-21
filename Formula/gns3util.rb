dfa49135e40a2c0eb1087a78536b403bdf25941e
Autcoder <150786670+Autcoder@users.noreply.github.com>
2025-09-22 00:40:15 +0200

    more prayging

--- Formula/gns3util.rb
+++ Formula/gns3util.rb
@@ -1,13 +1,6 @@
 class Gns3util < Formula
   desc "GNS3 API utility for managing GNS3v3 servers"
   homepage "https://github.com/Stefanistkuhl/gns3-api-util"
-  version "1.0.0"
-  license "GPL-3.0-or-later"
-
-  livecheck do
-    url :stable
-    strategy :github_latest
-  end
 
   if Hardware::CPU.arm?
     url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.0.0/gns3util-darwin-arm64.tar.gz"
@@ -17,15 +10,17 @@ class Gns3util < Formula
     sha256 "99a20203561a071600a48383c5b42affe6df6a17f80a1b276fc5ca438c342db6"
   end
 
+  version "1.0.0"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   def install
-    # Install the correct top-level binary and rename it to 'gns3util'
     binary = Hardware::CPU.arm? ? "gns3util-darwin-arm64" : "gns3util-darwin-amd64"
     bin.install binary => "gns3util"
-
-    # Ship provided shell completions if present
-    # bash_completion.install "completions/gns3util.bash" => "gns3util" if File.exist?("completions/gns3util.bash")
-    # zsh_completion.install "completions/_gns3util" if File.exist?("completions/_gns3util")
-    # fish_completion.install "completions/gns3util.fish" if File.exist?("completions/gns3util.fish")
   end
 
   test do
