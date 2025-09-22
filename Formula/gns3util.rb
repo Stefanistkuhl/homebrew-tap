634f15fe2aeb07595c7250995e328ff9b37e7275
Stefanistkuhl <ghgstefan@gmail.com>
2025-09-22 11:25:17 +0200

    v1.1.2

--- Formula/gns3util.rb
+++ Formula/gns3util.rb
@@ -3,14 +3,14 @@ class Gns3util < Formula
   homepage "https://github.com/Stefanistkuhl/gns3-api-util"
 
   if Hardware::CPU.arm?
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.1/gns3util-darwin-arm64.tar.gz"
-    sha256 "1b3f619935fc139258088c8ff0952e0b4bf8913a21d0ea377c888d5abfdebf14"
+    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.2/gns3util-darwin-arm64.tar.gz"
+    sha256 "6f5e43024dacc95e256409da785007d4ae842673afbd3712b02bd75f167e6ebc"
   else
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.1/gns3util-darwin-amd64.tar.gz"
-    sha256 "fe9da6e37ad61fe9bb8006f49d2ebd23877aff7490febdc3588757ba10ac2631"
+    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.2/gns3util-darwin-amd64.tar.gz"
+    sha256 "8a91129d7875885a929a594667a314e64f38edca678da8016bf299ebc042671b"
   end
 
-  version "1.1.1"
+  version "1.1.2"
   license "GPL-3.0-or-later"
 
   livecheck do
