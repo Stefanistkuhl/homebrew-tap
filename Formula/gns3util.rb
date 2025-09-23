868c13dfcfc835a43ace2b2f17caa24e2560147b
Stefanistkuhl <ghgstefan@gmail.com>
2025-09-23 10:47:51 +0200

    ermmies

--- Formula/gns3util.rb
+++ Formula/gns3util.rb
@@ -1,17 +1,19 @@
 class Gns3util < Formula
   desc "GNS3 API utility for managing GNS3v3 servers"
   homepage "https://github.com/Stefanistkuhl/gns3-api-util"
-
-  if Hardware::CPU.arm?
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-arm64.tar.gz"
-    sha256 "4d77d5da93fe581da27cf3357354efd105fe0c21c1d78de08922e14d456d7384"
-  else
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-amd64.tar.gz"
-    sha256 "8163e19a2df0b489366ef88f904abed320c7eebd2e9ae39ae1209d0a4f6d60a7"
-  end
-
   version "1.1.4"
   license "GPL-3.0-or-later"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-arm64.tar.gz"
+      sha256 "4d77d5da93fe581da27cf3357354efd105fe0c21c1d78de08922e14d456d7384"
+    end
+    on_intel do
+      url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-amd64.tar.gz"
+      sha256 "8163e19a2df0b489366ef88f904abed320c7eebd2e9ae39ae1209d0a4f6d60a7"
+    end
+  end
 
   livecheck do
     url :stable
@@ -19,8 +21,12 @@ class Gns3util < Formula
   end
 
   def install
-    binary = Hardware::CPU.arm? ? "gns3util-darwin-arm64" : "gns3util-darwin-amd64"
-    bin.install binary => "gns3util"
+    # After extraction, install the single binary regardless of name
+    bin_name = Dir["gns3util*"].find { |f| File.file?(f) && File.executable?(f) } ||
+               Dir["gns3util*"].first
+    odie "binary not found in archive" unless bin_name
+    chmod 0755, bin_name
+    bin.install bin_name => "gns3util"
   end
 
   test do
