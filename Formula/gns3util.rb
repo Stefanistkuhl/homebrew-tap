867adbab15fe26892b098a32c2082dac2befed89
Stefanistkuhl <ghgstefan@gmail.com>
2025-09-23 10:38:40 +0200

    remove whitespace

--- Formula/gns3util.rb
+++ Formula/gns3util.rb
@@ -3,10 +3,10 @@ class Gns3util < Formula
   homepage "https://github.com/Stefanistkuhl/gns3-api-util"
 
   if Hardware::CPU.arm?
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-arm64.tar.gz "
+    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-arm64.tar.gz"
     sha256 "4d77d5da93fe581da27cf3357354efd105fe0c21c1d78de08922e14d456d7384"
   else
-    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-amd64.tar.gz "
+    url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.1.4/gns3util-darwin-amd64.tar.gz"
     sha256 "8163e19a2df0b489366ef88f904abed320c7eebd2e9ae39ae1209d0a4f6d60a7"
   end
 
