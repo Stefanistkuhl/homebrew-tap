1a9635d8f29b5fc0802af2355fe1e147acb3a79f
Stefanistkuhl <ghgstefan@gmail.com>
2025-09-29 09:58:46 +0200

    v1.2.5

--- Formula/gns3util.rb
+++ Formula/gns3util.rb
@@ -1,17 +1,17 @@
 class Gns3util < Formula
   desc "GNS3 API utility for managing GNS3v3 servers"
   homepage "https://github.com/Stefanistkuhl/gns3-api-util"
-  version "1.2.4"
+  version "1.2.5"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.2.4/gns3util-darwin-arm64.tar.gz"
-      sha256 "4c76a0e54e0994bf384edd276cbcc765588f9a043cd61cba3c32dcde641c4218"
+      url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.2.5/gns3util-darwin-arm64.tar.gz"
+      sha256 "8f5c76ccd7e34fc30923d69c78f1cd3255e28b1694b7d497e3b4dfff3232e453"
     end
     on_intel do
-      url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.2.4/gns3util-darwin-amd64.tar.gz"
-      sha256 "aa0b42028bd39236206a113250e7bcb65937d19c2a694d5bf4348888dc166fe0"
+      url "https://github.com/Stefanistkuhl/gns3-api-util/releases/download/v1.2.5/gns3util-darwin-amd64.tar.gz"
+      sha256 "dcd4f10ddcb50a6892f28e7fbf34211c37ead1f1c911bc688c622fb44d3103e7"
     end
   end
 
